aa78aa1c4e4da97f14c5f7d6efeca5e2ba6161e6
xiaosha <panxiao0811@gmail.com>
2021-07-19 16:04:25 +0800

    # 后台路由

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -11,10 +11,16 @@ Route::group([
     'as'            => config('admin.route.prefix') . '.',
 ], function (Router $router) {
     $router->get('/', 'HomeController@index')->name('home');
+    // 管理员
     $router->resource('users', UserController::class);
+    // 资讯
     $router->resource('articles', ArticleController::class);
-    $router->resource('users', MemberController::class);
+    // 用户
+    $router->resource('members', MemberController::class);
+    // 商品
     $router->resource('goods', GoodsController::class);
+    // 积分
     $router->resource('integrals', IntegralController::class);
+    // 订单
     $router->resource('orders', OrderController::class);
 });
